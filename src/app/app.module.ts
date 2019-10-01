9cb17a8a9fb9d440cd04074beb93af33fcca73d2
suyashshukla <suyashshukla1997@gmail.com>
2019-10-01 10:52:13 +0530

    Linked

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
